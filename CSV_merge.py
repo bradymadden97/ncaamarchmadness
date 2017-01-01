147f4caecc853af3aff82ca00345028b686dbf48
Brady Madden <bradymadden97@gmail.com>
2016-12-31 20:16:40 -0500

    added seeds to master data csv

--- CSV_merge.py
+++
@@ -1,9 +0,0 @@
-#Brady Madden
-#Merges each sorted seeding CSV into master ncaa_tournament_data.csv to add seeding data to teams
-
-#Currently unfinished
-import csv
-
-with open('ncaa_tournament_data.csv', 'r') as f:
-    data = [line for line in csv.reader(f)]
-print(data)
